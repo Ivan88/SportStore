a8f6d80552769dcaf0db819d4b5836a6b79a6cd8
Ivan Omelchenko <oduvanchik22@gmail.com>
2018-02-09 09:55:12 +0200

    Add project files.

---
+++ SportStore.Domain/Abstract/IProductsRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using SportStore.Domain.Entities;
+
+namespace SportStore.Domain.Abstract
+{
+	public interface IProductsRepository
+	{
+		IEnumerable<Product> Products { get; }
+	}
+}
---
+++ SportStore.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SportStore.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SportStore.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1a391fc8-b91a-4bf1-8913-88550696c5b7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
